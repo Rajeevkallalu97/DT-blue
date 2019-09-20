d4b93d1bf3cc79ec65e07af4a20a563f34f23d16
rajeevkallalu <rajeevkallalu97@gmail.com>
2019-09-20 20:00:51 +1000

    null

---
+++ InspectPro/InspectPro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InspectPro
+//
+//  Created by Sai rajeev Kallalu on 20/9/19.
+//  Copyright © 2019 Sai rajeev Kallalu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ InspectPro/InspectProTests/InspectProTests.swift
@@ -0,0 +1,34 @@
+//
+//  InspectProTests.swift
+//  InspectProTests
+//
+//  Created by Sai rajeev Kallalu on 20/9/19.
+//  Copyright © 2019 Sai rajeev Kallalu. All rights reserved.
+//
+
+import XCTest
+@testable import InspectPro
+
+class InspectProTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
